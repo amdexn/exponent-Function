12abbe8a134bbf212d7f96e0d4c48c02ec57aa23
amdexn <nanyabis894@gmail.com>
2025-05-25 14:11:58 +0200

    added raise to power fx

--- App.py
+++ App.py
@@ -1,1 +1,11 @@
-print("Hello World")
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for i in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(2, 5))
+
+
+
