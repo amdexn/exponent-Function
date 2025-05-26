e76fb797c7bdc6067d48e15e9e169095617cfc6d
amdexn <nanyabis894@gmail.com>
2025-05-26 16:31:08 +0200

    Dictionaries

--- App.py
+++ App.py
@@ -1,11 +1,19 @@
 
-def raise_to_power(base_num, pow_num):
-    result = 1
-    for i in range(pow_num):
-        result = result * base_num
-    return result
-
-print(raise_to_power(2, 5))
+monthConversions = {
+    'Jan': "January",
+    'Feb': "February",
+    'Mar': "March",
+    'Apr': "April",
+    'May': "May",
+    'Jun': "June",
+    'Jul': "July",
+    'Aug': "August",
+    'Sep': "September",
+    'Oct': "October",
+    'Nov': "November",
+    'Dec': "December"
+}
+print(monthConversions.get("Dec", "not a valid Key"))
 
 
 
